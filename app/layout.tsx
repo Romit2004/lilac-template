6969e546893a2869185cf7a1a44704ccde4d84e3
Romit2004 <romitguha7@gmail.com>
2026-02-07 20:50:37 +0530

    fixed layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps): JSX.Element {
+export default function RootLayout({ children }: RootLayoutProps)  {
   return (
     <html lang="en">
       <body className="antialiased">{children}</body>
